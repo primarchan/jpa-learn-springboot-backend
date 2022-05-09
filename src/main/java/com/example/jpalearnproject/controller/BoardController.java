955bc6e28de718d170f0e4e37656c97f261c2012
primarchan <boltcarrier@naver.com>
2022-05-09 18:16:35 +0900

    add controller, service

---
+++ src/main/java/com/example/jpalearnproject/controller/BoardController.java
@@ -0,0 +1,12 @@
+package com.example.jpalearnproject.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/board")
+@RestController
+public class BoardController {
+
+
+}
---
+++ src/main/java/com/example/jpalearnproject/service/BoardService.java
@@ -0,0 +1,4 @@
+package com.example.jpalearnproject.service;
+
+public interface BoardService {
+}
